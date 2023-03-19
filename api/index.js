5306a9b047c9135aa892431775b1d766e726e80c
whwp <teyhawap@gmail.com>
2023-03-18 21:08:12 -0700

    commit

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const { v4 } = require("uuid");
+
+app.use(express.static("public"));
+
+//index.js
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
 
 app.get("/api", (req, res) => {
   const path = `/api/item/${v4()}`;
@@ -16,4 +26,5 @@ app.get("/api/item/:slug", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running.");
 });
-//module.exports = app;
+
+module.exports = app;
